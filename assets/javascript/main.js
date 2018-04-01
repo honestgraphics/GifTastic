6ca2809c8e8ab68621cefd724d2b16e5b48fe9cf
honestgraphics <jeannie@honestgraphics.com>
2018-04-01 16:26:17 -0400

    draft1

---
+++ assets/javascript/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  //topics array
+  var topics = ["schnauzer", "pug", "lab", "retriever", "jack russell", "scottish terrier"];
+
+  //variable to store buttons
+  var buttons = "";
+
+  //for loop to create buttons
+  for (var i = 0; i < topics.length; i++) {
+    
+    
+    
+    buttons += "<button type='button'>" + topics[i] + "here</button>"
+  }
+
+
+
+
+
+});
